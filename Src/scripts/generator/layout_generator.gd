81b82034b858b69ad472790cb748b1ddf9bcf4bb
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-03-31 16:51:32 +0200

    ADD: implémentation du générateur de plan de labyrinthe (recursive backtracker/depth-first search)

--- Src/scripts/generator/layout_generator.gd
+++ Src/scripts/generator/layout_generator.gd
@@ -1,9 +1,92 @@
 extends TileMap
 
+const N = 1
+const E = 2
+const S = 4
+const W = 8
+
+var cell_walls = {
+	Vector2(+0, -1): N,
+	Vector2(+1, +0): E,
+	Vector2(+0, +1): S,
+	Vector2(-1, +0): W,
+}
+
+#var size : Vector2 = Vector2(20, 18)
+var size : Vector2 = Vector2(10, 9)
+var passthrough_fraction : float = 0.45
+
 func _ready():
-	clear()
-	set_cellv(Vector2(0,0),0)
+	randomize()
+	make_maze()
 	pass
-	
+
+func check_neighbors(cell, unvisited):
+	var list = []
+	for n in cell_walls.keys():
+		if cell + n in unvisited:
+			list.append(cell + n)
+	return list
+
+func make_maze():
+	var wait = 1
+	var iteration = 0
+	var max_iteration = 25
+	while true:
+		passthrough_fraction = (randf() * 0.5) + 0.05
+		clear()
+		var unvisited = []
+		var map = []
+		var stack = []
+		
+		for x in range(size.x):
+			for y in range(size.y):
+				unvisited.append(Vector2(x, y))
+				map.append(Vector2(x, y))
+				set_cellv(Vector2(x, y), N|E|S|W)
+				
+		var current : Vector2 = unvisited[randi() % unvisited.size()]
+		unvisited.erase(current)
+		
+		while unvisited:
+			var neighbors = check_neighbors(current, unvisited)
+			if neighbors.size() > 0:
+				var next = neighbors[randi() % neighbors.size()]
+				stack.append(current)
+				var direction : Vector2 = next - current
+				var new_current_walls_mask = get_cellv(current) - cell_walls[direction]
+				var new_next_walls_mask = get_cellv(next) - cell_walls[-direction]
+				set_cellv(current, new_current_walls_mask)
+				set_cellv(next, new_next_walls_mask)
+				
+				current = next
+				unvisited.erase(current)
+			elif stack:
+				current = stack.pop_back()
+		
+		for i in range(int(size.x * size.y * passthrough_fraction)):
+			var cell : Vector2 = map[randi() % map.size()]
+			var next = Vector2(0, 0)
+			
+			while next.length() == 0 || not (cell + next in map):
+				next = cell_walls.keys()[randi() % cell_walls.size()]
+			
+			if get_cellv(cell) & cell_walls[next]:
+				var cell_wall_mask = get_cellv(cell) - cell_walls[next]
+				var next_cell_wall_mask = get_cellv(cell + next) - cell_walls[-next]
+				set_cellv(cell, cell_wall_mask)
+				set_cellv(cell + next, next_cell_wall_mask)
+				
+		
+		
+		iteration = (iteration + 1) % max_iteration
+		if iteration == 0:
+			max_iteration = (randi() % 10) + 6
+			wait = (randi() % 25) + 25
+		else:
+			wait = 1
+		for n in range(wait):
+				yield(get_tree(), "idle_frame")
+				
 func _physics_process(delta):
 	pass
